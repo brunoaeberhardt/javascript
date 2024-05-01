b1fbfbdf672f71c778dfee467f91e5f00e0a2ffe
Bruno Araujo Eberhardt <168445354+brunoaeberhardt@users.noreply.github.com>
2024-04-30 23:13:17 -0300

    Initial commit

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`) //Teste lógico no final
+    c++
+} while (c <= 6)
+
+/*
+while (c <= 6) {
+    console.log(`Passo ${c}`)   //Teste lógico no inicio
+    c++
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')  //Modo Josnel
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ bruno/script.js
@@ -0,0 +1,4 @@
+function pesquisar(){
+    var barra = document.getElementById('txtgo')
+    var res = document.getElementById('verf')
+}
