966c9ed88c09d453b07117d8a9c7811f7dc3f079
Bruno Araujo Eberhardt <168445354+brunoaeberhardt@users.noreply.github.com>
2024-06-11 10:36:37 -0300

    Exercícios

---
+++ aula13/ambiente1.js
@@ -0,0 +1,29 @@
+var c = 1
+
+do { //Estrutura de repetição com tetse lógico no final
+    console.log(`Passo ${c}`)
+    c = c + 1 
+} while (c <= 6)
+
+
+
+
+
+
+/* while (c <= 10) {  Estrutura de repetição com teste lógico no inicio
+    console.log(`Passo ${c}`)
+    c = c + 1
+} */
+
+
+
+
+
+
+
+/* console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')                   MODO JOSNEL
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
---
+++ aula14/ambiente1.js
@@ -0,0 +1,9 @@
+var c = 1
+for(c = 1; c <= 8; c = c + 1) {
+    console.log(`O grêmio é gigante, ${c}º vez.`)
+}
+var p = 1
+do {
+    console.log(`O grêmio irá ganhar a libertadores, ${p}º vez.`)
+    p = p + 1
+} while(p <= 6)
